ed601adbaf5d8e434aa3604ae2c27aa040415894
Petr Myazin <petr.myazin@gmail.com>
2020-07-18 12:31:18 +0300

    Add PHPUnit test

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require "src/Differ/Differ.php";
-
-$diff = \Differ\Differ\genDiff("before.json", "after.json");
-print_r($diff);
---
+++ tests/Differ/DifferTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use function Differ\Differ\genDiff;
+
+class DifferTest extends TestCase
+{
+    public function testGenDiff()
+    {
+        $file1 = __DIR__ . '/fixtures/1.json';
+        $file2 = __DIR__ . '/fixtures/2.json';
+
+        $diff = genDiff($file1, $file2);
+
+        $expectedDiff = <<<DIFF
+{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: 1
+}
+DIFF;
+
+        $this->assertEquals($expectedDiff, $diff);
+
+    }
+}
